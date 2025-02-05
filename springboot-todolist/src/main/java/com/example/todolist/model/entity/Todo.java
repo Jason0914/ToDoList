852dde9a55b399f4b281eb0b133a0230f520696b
Jason0914 <a22586203a@gmail.com>
2025-02-05 23:23:30 +0800

    model、repository

---
+++ springboot-todolist/src/main/java/com/example/todolist/model/entity/Todo.java
@@ -0,0 +1,26 @@
+package com.example.todolist.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //自動生成getter/setter方法，toString()，equals、hashCode方法
+@AllArgsConstructor //自動生成包含所有參數的建構子
+@NoArgsConstructor //自動生成無參數的建構子
+@Entity //標記這個類別是一個實體類，會對應到資料庫的表格
+//@Table(name="todo")//預設名稱，指定資料表名稱
+public class Todo {
+	
+	@Id//標記這個欄位是主鍵
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//由資料庫來決定id生成策略，主鍵生成策略
+	private Long id;
+	
+	private String text;
+	
+	private Boolean completed;
+
+}

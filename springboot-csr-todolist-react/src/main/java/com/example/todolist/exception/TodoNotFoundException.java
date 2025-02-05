10dfa06459eb548e3b26861d7523140200eb4fb7
Jason0914 <a22586203a@gmail.com>
2025-02-05 21:50:51 +0800

    這才是正確後端代碼

--- springboot-csr-todolist-react/src/main/java/com/example/todolist/exception/TodoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.todolist.exception;
-
-// 自訂例外(受檢例外)
-public class TodoNotFoundException extends Exception {
-	public TodoNotFoundException(String message) {
-		super(message);
-	}
-}
